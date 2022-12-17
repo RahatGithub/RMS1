6482d918cfefc169dc061becdd637fc5b88349a0
Rahat Ahmed Chowdhury <rahatahmedchowdhuryrac@gmail.com>
2022-12-17 23:58:37 +0600

    Basic models are added

--- main/models.py
+++ main/models.py
@@ -1,3 +1,41 @@
 from django.db import models
 
-# Create your models here.
+
+class Batch(models.Model):
+    id = models.AutoField
+    batch_id = models.CharField(max_length=18, default="")  # e.g. "CSE-12_2018-2019"
+    batch_no = models.CharField(max_length=6, default="")   # e.g. "CSE-12"
+    session = models.CharField(max_length=8, default="")    # e.g. "2018-2019"
+    students_json = models.CharField(max_length=5000, default="")       # e.g. {'2018331500' : {'name':'john doe', 'batch':'CSE-12_2018-2019'...} }
+    
+
+class Semester(models.Model):
+    id = models.AutoField 
+    semester_id =  models.CharField(max_length=14, default="")
+    semester_no = models.IntegerField(default=None)
+    courses_json = models.CharField(max_length=5000, default="")
+
+   
+class Course(models.Model):
+    id = models.AutoField
+    course_id = models.CharField(max_length=20, default="")
+    course_code = models.CharField(max_length=8, default="")
+    course_title = models.CharField(max_length=60, default="")
+    course_type =  models.CharField(max_length=10, default="")
+    course_credits = models.FloatField(default=0) 
+    course_teacher = models.CharField(max_length=40, default="") 
+    course_results_json = models.CharField(max_length=5000, default="")  # e.g. {'2018331500' : {'GP':'3.75', 'LG':'A'}} 
+
+
+class Student(models.Model):
+    id = models.AutoField 
+    reg_no = models.CharField(max_length=10, default="")
+    name = models.CharField(max_length=60, default="")
+    father_name = models.CharField(max_length=60, default="")
+    mother_name = models.CharField(max_length=60, default="")
+    address = models.CharField(max_length=100, default="")
+    phone = models.CharField(max_length=11, default="")
+    isResidential = models.BooleanField(default=False)
+    isCR = models.BooleanField(default=False) 
+    average_cgpa = models.FloatField(default=0)
+    remarks = models.CharField(max_length=500, default="")
