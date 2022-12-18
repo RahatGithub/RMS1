285401470bfda042bb6b23524f6885e2d96be663
Rahat Ahmed Chowdhury <rahatahmedchowdhuryrac@gmail.com>
2022-12-18 14:57:40 +0600

    Admin/stuff login applied in accounts.views

--- accounts/views.py
+++ accounts/views.py
@@ -32,15 +32,28 @@ def register(request):
 
 
 def login(request):
+    form_name = request.POST.get('form_name')
     if request.method == "POST":
-        username = request.POST['username']
-        password = request.POST['password']
-        user = auth.authenticate(request, username=username, password=password)
-        if user:
-            auth.login(request, user)
-            return redirect("/main/")
-        else:           
-            return render(request, "accounts/login.html")
+        if form_name == 'admin_form':
+            email = request.POST['admin_email']
+            password = request.POST['admin_password']
+            username = User.objects.filter(email=email)[0].username
+            user = auth.authenticate(request, username=username, password=password)
+            if user:
+                auth.login(request, user)
+                return redirect("/main/")
+            else:           
+                return render(request, "accounts/login.html")
+        elif form_name == 'stuff_form':
+            email = request.POST['stuff_email']
+            password = request.POST['stuff_password']
+            username = User.objects.filter(email=email)[0].username
+            user = auth.authenticate(request, username=username, password=password)
+            if user:
+                auth.login(request, user)
+                return redirect("/main/")
+            else:           
+                return render(request, "accounts/login.html")
     return render(request, "accounts/login.html")
 
 
