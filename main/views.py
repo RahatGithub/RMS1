5126cb8a232c359d2442f6c603a0d83d34a9bb79
Rahat Ahmed Chowdhury <rahatahmedchowdhuryrac@gmail.com>
2022-12-23 00:12:35 +0600

    Tabulation sheet logic attempt-2

--- main/views.py
+++ main/views.py
@@ -57,50 +57,38 @@ def semester_view(request, batch_no, semester_no):
     table_sheet = list()   # a list to store all result records for all students in this semester
     
     for student in students:
-        result_entry = dict()   # a dictionary to store result record for each student in this semster
-        
+        result_entry = dict()   # a dictionary to store result record for each student in this semester
         reg_no = student.reg_no
-        # print("name:", student.name)
-        # print("reg:", student.reg_no)
         result_entry['reg_no'] = reg_no
         result_entry['name'] = student.name
         
         total_credits = 0
         total_GP = 0
         final_LG = None
+        
         for course in courses:
-            # ******* Taking theory and sessional result of a student ******
-            if course.course_type == "Theory":
-                try:
-                    course_result = TheoryCourseResult.objects.get(batch_no=batch_no, semester_no=semester_no, reg_no=reg_no)
-                except: 
-                    course_result = None
-            elif course.course_type == "Sessional":
-                try:
-                    course_result = SessionalCourseResult.objects.get(batch_no=batch_no, semester_no=semester_no, reg_no=reg_no)
-                except: 
-                    course_result = None
-            # _______________________________________________________________
             try: 
-                result_entry[course.course_code] = {'credits' : course.course_credits,
-                                                    'GP' : course_result.GP,
-                                                    'LG' : course_result.LG }
+                course_result = TheoryCourseResult.objects.get(batch_no=batch_no, semester_no=semester_no, course_code=course.course_code, reg_no=reg_no)
             except:
-                result_entry[course.course_code] = {'credits' : course.course_credits,
-                                                    'GP' : None,
-                                                    'LG' : None }
-
+                course_result = SessionalCourseResult.objects.get(batch_no=batch_no, semester_no=semester_no, course_code=course.course_code, reg_no=reg_no)
+            
+            result_entry[course.course_code] = {'credits' : course.course_credits,
+                                                'GP' : course_result.GP, 
+                                                'LG' : course_result.LG}
             total_credits += course.course_credits
-            total_GP += course.course_credits * course_result.GP 
+            total_GP += course.course_credits * float(course_result.GP) 
         
-        final_GP = total_GP / total_credits
-        result_entry[f"semester {semester_no}"] = {'credits' : total_credits,
-                                                   'GP' : final_GP,
-                                                   'LG' : 'X'}
+        final_GPA =  round((total_GP/total_credits), 2) 
+        result_entry[f"semester {semester_no}"] = {'total_credits' : total_credits,
+                                                   'final_GPA' : final_GPA,
+                                                   'final_LG' : 'X'}
         
+        table_sheet.append(result_entry)
+        
+    # _____finally printing the output________
     for record in table_sheet:
         print(record)
-        
+    
         
     print("*"*30)
     # ******************
