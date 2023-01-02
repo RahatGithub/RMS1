4c1527ac6f83181bd53ca0a6a8b214619275c86e
Rahat Ahmed Chowdhury <rahatahmedchowdhuryrac@gmail.com>
2023-01-02 23:27:54 +0600

    Gradesheet HTML ok

--- main/urls.py
+++ main/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('students_view/<str:batch_no>', views.students_view, name="students_view"),
     path('add_semester/<str:batch_no>', views.add_semester, name="add_semester"),
     path('course_view/<str:batch_no>/<int:semester_no>/<str:course_type>/<str:course_code>', views.course_view, name="course_view"),
-    path('gradesheet_view/<str:session>/<str:reg_no>', views.gradesheet_view, name="gradesheet_view") 
+    path('gradesheet_view/<str:session>/<str:reg_no>', views.gradesheet_view, name="gradesheet_view"),
+    # path('pdf_view/', views.ViewPDF.as_view(), name="pdf_view")
 ]
--- main/views.py
+++ main/views.py
@@ -250,6 +250,22 @@ def students_view(request, batch_no):
     return render(request, 'main/students_view.html', {'batch_no':batch_no, 'students':students})
 
 
+# ******************* experimental ***********************
+# from io import BytesIO
+# from django.template.loader import get_template
+# from xhtml2pdf import pisa
+# from django.views import View
+
+# def render_to_pdf(template_src, context_dict={}):
+#     template = get_template(template_src)
+#     html  = template.render(context_dict)
+#     result = BytesIO()
+#     pdf = pisa.pisaDocument(BytesIO(html.encode("ISO-8859-1")), result)
+#     if not pdf.err:
+#         return HttpResponse(result.getvalue(), content_type='application/pdf')
+#     return None
+# *********************************************************
+
 def gradesheet_view(request, session, reg_no):
     batch = Batch.objects.get(session=session)
     batch_no = batch.batch_no
@@ -258,28 +274,64 @@ def gradesheet_view(request, session, reg_no):
     
     gradesheet = list()
     semester_results = Result.objects.filter(reg_no=reg_no)
+    cumulative_credits, cumulative_point, cumulative_LG = float(0), float(0), 'NA'   # for the overall result of all semesters
     for result in semester_results:
-        a_semester = list()
+        a_semester = dict()
         course_results = json.loads(result.course_results)
-        # cumulative_credits, cumulative_point, cumulative_LG = float(0), float(0), 'NA'
+        course_results_info = list()
+        this_semester_credits, this_semester_point, this_semester_LG = float(0), float(0), 'NA'   # for only a particular semester
         for cour in course_results.items():
-            course_info = dict()
-            course_info['course_code'] = cour[0]
+            course_info = list()
+            course_info.append(cour[0])     # pushing the course_code at index=0
             course = Course.objects.filter(batch_no=batch_no, course_code=cour[0]).first() 
-            course_info['course_title'] = course.course_title
-            course_info['credits'] = course.course_credits
-            course_info['GP'] = cour[1]['GP']
-            course_info['LG'] = cour[1]['LG']
-            # if course_info['GP'] >= 2: 
-            #     cumulative_credits += course_info['credits']
-            #     cumulative_point += course_info['GP'] * course_info['credits']
+            course_info.append(course.course_title)    # pushing the course_title at index=1
+            course_info.append(course.course_credits)  # pushing the course_credits at index=2
+            course_info.append(cour[1]['GP'])      # pushing the obtained GP at index=3
+            course_info.append(cour[1]['LG'])      # pushing the obtained LG at index=4
+            if float(course_info[3]) >= 2:            # checking if the obtained GP >= 2 
+                this_semester_credits += course_info[2]  # cumulative_credits += course_credits
+                this_semester_point += float(course_info[3]) * course_info[2]  # cumulative_point += GP * course_credits
+            course_results_info.append(course_info)
+        
+        a_semester['this_semester_credits'] = this_semester_credits
+        a_semester['course_results_info'] = course_results_info
             
-            a_semester.append(course_info)
-
-            # cumulative_GP = round((cumulative_point/cumulative_credits), 2)
+        this_semester_GP = round((this_semester_point/this_semester_credits), 2)
+        a_semester['this_semester_GP'] = this_semester_GP
             
-        gradesheet.append(a_semester)        
+        if this_semester_GP == 4.00 : this_semester_LG = "A+"
+        elif this_semester_GP >= 3.75 : this_semester_LG = "A"
+        elif this_semester_GP >= 3.50 : this_semester_LG = "A-" 
+        elif this_semester_GP >= 3.25 : this_semester_LG = "B+"
+        elif this_semester_GP >= 3.00 : this_semester_LG = "B"
+        elif this_semester_GP >= 2.75 : this_semester_LG = "B-"
+        elif this_semester_GP >= 2.50 : this_semester_LG = "C+"
+        elif this_semester_GP >= 2.25 : this_semester_LG = "C"
+        elif this_semester_GP >= 2.00 : this_semester_LG = "C-" 
+        else: this_semester_LG = "F"
+        a_semester['this_semester_LG'] = this_semester_LG
 
-    # for gs in gradesheet : print(gs, end="\n\n")
+        cumulative_credits += this_semester_credits
+        a_semester['cumulative_credits'] = cumulative_credits
+        cumulative_point += this_semester_point
+        cumulative_GP = round((cumulative_point/cumulative_credits), 2)
+        a_semester['cumulative_GP'] = cumulative_GP
+        
+        if cumulative_GP == 4.00 : cumulative_LG = "A+"
+        elif cumulative_GP >= 3.75 : cumulative_LG = "A"
+        elif cumulative_GP >= 3.50 : cumulative_LG = "A-" 
+        elif cumulative_GP >= 3.25 : cumulative_LG = "B+"
+        elif cumulative_GP >= 3.00 : cumulative_LG = "B"
+        elif cumulative_GP >= 2.75 : cumulative_LG = "B-"
+        elif cumulative_GP >= 2.50 : cumulative_LG = "C+"
+        elif cumulative_GP >= 2.25 : cumulative_LG = "C"
+        elif cumulative_GP >= 2.00 : cumulative_LG = "C-" 
+        else: cumulative_LG = "F"
+        a_semester['cumulative_LG'] = cumulative_LG
+        
+        
+        gradesheet.append(a_semester)      
+
+    for gs in gradesheet : print(gs, end="\n\n")
         
     return render(request, 'main/gradesheet_view.html', {'session':session, 'reg_no':reg_no, 'student_name':student_name, 'gradesheet':gradesheet})
